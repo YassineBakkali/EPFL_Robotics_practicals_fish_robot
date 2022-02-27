1c812e3d7efbd11b089ea10afdf3cc80b888bd42
nguyen-v <vincen7.nguyen@gmail.com>
2022-02-27 11:01:32 +0100

    Added led blinking in green at 1 Hz (ex 1)

--- robot/ex1/main.c
+++ robot/ex1/main.c
@@ -9,31 +9,39 @@ int main(void)
   hardware_init();
   reg32_table[REG32_LED] = LED_MANUAL;  // manual LED control
 
+  // while (1) {
+  //   for (i = 0; i < 127; i++) {
+  //     set_rgb(i, 0, 0);
+  //     pause(TEN_MS);
+  //   }
+  //   for (i = 0; i < 127; i++) {
+  //     set_rgb(127, i, 0);
+  //     pause(TEN_MS);
+  //   }
+  //   for (i = 0; i < 127; i++) {
+  //     set_rgb(127, 127, i);
+  //     pause(TEN_MS);
+  //   }
+  //   for (i = 127; i >= 0; i--) {
+  //     set_rgb(i, 127, 127);
+  //     pause(TEN_MS);
+  //   }
+  //   for (i = 127; i >= 0; i--) {
+  //     set_rgb(0, i, 127);
+  //     pause(TEN_MS);
+  //   }
+  //   for (i = 127; i >= 0; i--) {
+  //     set_rgb(0, 0, i);
+  //     pause(TEN_MS);
+  //   }
+  // }
+  
+  // Make the LED blink in green at 1 Hz
   while (1) {
-    for (i = 0; i < 127; i++) {
-      set_rgb(i, 0, 0);
-      pause(TEN_MS);
-    }
-    for (i = 0; i < 127; i++) {
-      set_rgb(127, i, 0);
-      pause(TEN_MS);
-    }
-    for (i = 0; i < 127; i++) {
-      set_rgb(127, 127, i);
-      pause(TEN_MS);
-    }
-    for (i = 127; i >= 0; i--) {
-      set_rgb(i, 127, 127);
-      pause(TEN_MS);
-    }
-    for (i = 127; i >= 0; i--) {
-      set_rgb(0, i, 127);
-      pause(TEN_MS);
-    }
-    for (i = 127; i >= 0; i--) {
-      set_rgb(0, 0, i);
-      pause(TEN_MS);
-    }
+    set_rgb(0, 127, 0);
+    pause(5*HUNDRED_MS);
+    set_rgb(0, 0, 0);
+    pause(5*HUNDRED_MS);
   }
   return 0;
 }
